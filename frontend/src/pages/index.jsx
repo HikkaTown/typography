518b64338742ad18edb8c677a553c4de0efa7f6a
Gennady Zaitsev <hikka.down@yandex.ru>
2022-04-12 10:13:07 +0300

    Добавил жёлтую кнопку

--- frontend/src/pages/index.jsx
+++ frontend/src/pages/index.jsx
@@ -2,6 +2,7 @@ import ArrowBtn from "../components/uikit/ArrowBtn/ArrowBtn";
 import BtnNext from "../components/uikit/BtnNext/BtnNext";
 import CloseBtn from "../components/uikit/CloseBtn/CloseBtn";
 import Tab from "../components/uikit/Tab/Tab";
+import YellowBtn from "../components/uikit/YellowBtn/YellowBtn";
 
 export default function Index() {
   return (
@@ -11,6 +12,7 @@ export default function Index() {
       <CloseBtn />
       <Tab isActive={true}>Широкоформатная печать</Tab>
       <Tab>Широкоформатная печать</Tab>
+      <YellowBtn>В каталог услуг</YellowBtn>
     </>
   );
 }
