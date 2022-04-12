ea98cb7aeed27fdf2d35ba7492ded68d8d9f5df0
Gennady Zaitsev <hikka.down@yandex.ru>
2022-04-12 09:59:47 +0300

    Добавил кнопку таба

---
+++ frontend/src/components/uikit/Tab/Tab.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import s from "./Tab.module.scss";
+import cs from "classnames";
+export default function Tab({
+  className,
+  onClick,
+  children,
+  isActive = false,
+}) {
+  return (
+    <button className={cs(s.btn, isActive ? s.btn_active : "", className)}>
+      {children}
+    </button>
+  );
+}
--- frontend/src/pages/index.jsx
+++ frontend/src/pages/index.jsx
@@ -1,11 +1,16 @@
 import ArrowBtn from "../components/uikit/ArrowBtn/ArrowBtn";
 import BtnNext from "../components/uikit/BtnNext/BtnNext";
+import CloseBtn from "../components/uikit/CloseBtn/CloseBtn";
+import Tab from "../components/uikit/Tab/Tab";
 
 export default function Index() {
   return (
     <>
       <BtnNext />
       <ArrowBtn />
+      <CloseBtn />
+      <Tab isActive={true}>Широкоформатная печать</Tab>
+      <Tab>Широкоформатная печать</Tab>
     </>
   );
 }
