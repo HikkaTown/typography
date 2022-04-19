27c775b4d29d69494a8f990e0de1c228efa657ca
Gennady Zaitsev <hikka.down@yandex.ru>
2022-04-19 20:56:35 +0300

    иконки для bg в секции обратной связи

--- frontend/src/components/CallbackProudctSection/CallbackProudctSection.jsx
+++ frontend/src/components/CallbackProudctSection/CallbackProudctSection.jsx
@@ -4,6 +4,7 @@ import FeedbackModal from "../FeedbackModal/FeedbackModal";
 import { LazyImage } from "../LazyImage/LazyImage";
 import Button2 from "../uikit/Button2/Button2";
 import s from "./CallbackProudctSection.module.scss";
+import { Line, Mails, Smile, Star } from "./components/Icons";
 export default function CallbackProudctSection() {
   const [isOpened, setIsOpened] = useState(false);
   const handleOpen = () => {
@@ -12,12 +13,13 @@ export default function CallbackProudctSection() {
   return (
     <section className={s.section}>
       <div className={s.container}>
-        <LazyImage
-          src="/assets/CallbackProductSection/bg_desktop.jpg"
-          srcTablet="/assets/CallbackProductSection/bg_tab.jpg"
-          srcMobile="/assets/CallbackProductSection/bg_mob.jpg"
-          className={[s.background]}
-        />
+        <Smile className={s.smile} />
+        <Line className={s.first_line} />
+        <Line className={s.second_line} />
+        <Mails className={s.first_mails} />
+        <Mails className={s.second_mails} />
+        <Star className={s.first_star} />
+        <Star className={s.second_star} />
         <h2 className={s.header}>
           Все вопросы уточняйте у менеджера, оставьте заявку и мы свяжемся
           с вами в ближайщее время
