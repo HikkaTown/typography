7a37a3c9c17aa3cce26a3785f40f0d8f59f3af3e
Gennady Zaitsev <hikka.down@yandex.ru>
2022-04-25 17:13:52 +0300

    backups

--- backend/config/server.js
+++ backend/config/server.js
@@ -1,7 +1,20 @@
+const fs = require("fs");
+
 module.exports = ({ env }) => ({
   host: env("HOST", "0.0.0.0"),
   port: env.int("PORT", 1337),
   app: {
     keys: env.array("APP_KEYS"),
+  },
+  admin: {
+    watchIgnoreFiles: ["/private/*"],
+  },
+  cron: {
+    enabled: true,
+    tasks: {
+      "*/40 * * * * *": () => {
+        console.log("evry 40 second");
+      },
+    },
   },
 });
