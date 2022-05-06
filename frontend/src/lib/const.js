4bec73be03c7ed1e8d89f0022fcf59a296c4f734
Gennady Zaitsev <hikka.down@yandex.ru>
2022-05-06 23:36:58 +0300

    test build

--- frontend/src/lib/const.js
+++ frontend/src/lib/const.js
@@ -1,5 +1,5 @@
-export const URLAPI = "http://localhost:1337/graphql";
-export const PATH_IMAGE = "http://localhost:1337";
+export const URLAPI = "http://188.120.251.179:1337/graphql";
+export const PATH_IMAGE = "http://188.120.251.179:1337";
 export const DOMAIN = "http://localhost:3000";
 
 export const PAGES = ["/catalog", "/blog", "/projects", "/contacts"];
