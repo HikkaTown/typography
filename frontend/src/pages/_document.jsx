36a476baf011767961e5338c7630edd97923499d
Gennady Zaitsev <hikka.down@yandex.ru>
2022-06-17 12:14:01 +0300

    fix favicons

--- frontend/src/pages/_document.jsx
+++ frontend/src/pages/_document.jsx
@@ -25,53 +25,53 @@ class MyDocument extends Document {
           <link
             rel="apple-touch-icon"
             sizes="72x72"
-            href="favicons/apple-touch-icon-72x72.png"
+            href="/favicons/apple-touch-icon-72x72.png"
           />
           <link
             rel="apple-touch-icon"
             sizes="76x76"
-            href="favicons/apple-touch-icon-76x76.png"
+            href="/favicons/apple-touch-icon-76x76.png"
           />
           <link
             rel="apple-touch-icon"
             sizes="114x114"
-            href="favicons/apple-touch-icon-114x114.png"
+            href="/favicons/apple-touch-icon-114x114.png"
           />
           <link
             rel="apple-touch-icon"
             sizes="120x120"
-            href="favicons/apple-touch-icon-120x120.png"
+            href="/favicons/apple-touch-icon-120x120.png"
           />
           <link
             rel="apple-touch-icon"
             sizes="144x144"
-            href="favicons/apple-touch-icon-144x144.png"
+            href="/favicons/apple-touch-icon-144x144.png"
           />
           <link
             rel="apple-touch-icon"
             sizes="152x152"
-            href="favicons/apple-touch-icon-152x152.png"
+            href="/favicons/apple-touch-icon-152x152.png"
           />
           <link
             rel="apple-touch-icon"
             sizes="180x180"
-            href="favicons/apple-touch-icon-180x180.png"
+            href="/favicons/apple-touch-icon-180x180.png"
           />
           <link
             rel="icon"
-            href="favicons/favicon.png"
+            href="/favicons/favicon.png"
             sizes="16x16"
             type="image/png"
           />
           <link
             rel="icon"
-            href="favicons/favicon.ico"
+            href="/favicons/favicon.ico"
             sizes="16x16 32x32"
             type="image/vnd.microsoft.icon"
           />
           <link
             rel="icon"
-            href="favicons/favicon.svg"
+            href="/favicons/favicon.svg"
             sizes="any"
             type="image/svg+xml"
           />
