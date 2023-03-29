08b8fdce3712bde78bd88bb91764b8e8444e6ac4
Gennady <hikka.down@yandex.ru>
2023-03-29 19:42:18 +0300

    sitemap

--- frontend/src/lib/const.js
+++ frontend/src/lib/const.js
@@ -2,4 +2,4 @@ export const URLAPI = "https://api.1ppc.ru/graphql";
 export const PATH_IMAGE = "https://api.1ppc.ru";
 export const DOMAIN = "https://1ppc.ru";
 
-export const PAGES = ["/catalog", "/blog", "/projects", "/contacts"];
+export const PAGES = ["/catalog", "/blog", "/projects", "/contacts", "/landing-page"];
